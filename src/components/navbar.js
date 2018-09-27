03b16671d0d002a2b660cad9204db8fd2af11a82
Nurha <nurha@projectcodex.co>
2018-09-27 21:38:21 +0200

    Added menu bar.

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import AppBar from 'material-ui/AppBar';
 
 export default class NavBar extends Component {
     render() {
         return (
             <div>
-                <p>Blah for now</p>
+                <AppBar 
+                    title='Menu'
+                />
             </div>
         )
     }
