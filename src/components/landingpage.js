5bb5b90ee4e421014080a0687dbb1d648b939f09
Nurha <nurha@projectcodex.co>
2018-09-28 11:11:42 +0200

    Linked landing page button to the todolist page.

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
 
 export default class LandingPage extends Component {
+
+    navigate = (path) => {
+        this.props.history.push(path)
+    }
+
     render () {
         return (
             <div>
                 <h1>Welcome to your To Do List</h1>
+                <RaisedButton label="Launch" secondary={true} style={{margin: '12px'}} onClick={() => { this.navigate('/todolist')}} />
             </div>
         )
     }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,7 +6,7 @@ export default class NavBar extends Component {
         return (
             <div>
                 <AppBar 
-                    title='Menu'
+                    
                 />
             </div>
         )
